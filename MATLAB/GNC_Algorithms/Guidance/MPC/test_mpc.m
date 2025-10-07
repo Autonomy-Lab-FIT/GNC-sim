3f711672d852b9796583bb184b05da3847095492
bgopu2023 <bgopu2023@my.fit.edu>
2025-10-07 06:19:30 -0400

    Remove files from repo

--- MATLAB/GNC_Algorithms/Guidance/MPC/test_mpc.m
+++
@@ -1,9 +0,0 @@
-% Connect to your PX4 bridge
-config = px4_get_config();
-client = px4_connect(config.ip_address, config.port);
-
-% Run the point stabilization test for mpc
-run_mpc_point_stabilization_test(config, client);
-
- % Cleanup
-px4_cleanup(client);
--- MATLAB/GNC_Algorithms/Navigation/estimators/ukf/SigmaPoints.m
+++
@@ -1,6 +0,0 @@
-% calculate the sigma points
-function chi = SigmaPoints(c, P, x)
-    A = c*chol(P)'; % sqrt((n + lambda) * P)' 6x6
-    Y = x(:,ones(1,numel(x))); % expanding state vector from 6x1 to 6x6
-    chi = [x Y+A Y-A]; % sigma points 
-end
